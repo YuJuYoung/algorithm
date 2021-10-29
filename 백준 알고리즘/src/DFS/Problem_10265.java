ee0dff851c10d0f2c5fcaa72dd3310dfe2ea036c
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2021-10-29 21:26:56 +0900

    2250, 10265번 풀이중

--- 백준 알고리즘/src/DFS/Problem_10265.java
+++ 백준 알고리즘/src/DFS/Problem_10265.java
@@ -8,7 +8,6 @@ import java.util.StringTokenizer;
 public class Problem_10265 {
 	
 	private static int n, k;
-	private static Node[] nodes;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,23 +19,7 @@ public class Problem_10265 {
 		if (n <= k) {
 			System.out.println(n);
 		} else {
-			nodes = new Node[n + 1];
-			st = new StringTokenizer(br.readLine());
 			
-			for (int i = 1; i <= n; i++) {
-				int num = Integer.parseInt(st.nextToken());
-				nodes[num] = new Node(i, nodes[num]);
-			}
-		}
-	}
-	
-	private static class Node {
-		int n;
-		Node next;
-		
-		public Node(int n, Node node) {
-			this.n = n;
-			next = node;
 		}
 	}
 
---
+++ 백준 알고리즘/src/DFS/Problem_2250.java
@@ -0,0 +1,10 @@
+package DFS;
+
+public class Problem_2250 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
