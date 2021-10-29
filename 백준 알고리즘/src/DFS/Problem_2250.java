c9b474f93a7113fd4001c6a11a8a88104ad5f60a
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2021-10-29 21:27:24 +0900

    2250번 풀이중

--- 백준 알고리즘/src/DFS/Problem_2250.java
+++ 백준 알고리즘/src/DFS/Problem_2250.java
@@ -1,10 +1,46 @@
 package DFS;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Problem_2250 {
+	
+	private static int[][] tree;
+	private static int[][] result;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		tree = new int[N + 1][2];
+		result = new int[N][2];
+		
+		while (N-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int parent = Integer.parseInt(st.nextToken());
+			int left = Integer.parseInt(st.nextToken());
+			int right = Integer.parseInt(st.nextToken());
+			
+			tree[parent][0] = left;
+			tree[parent][1] = right;
+		}
+	}
+	
+	private static int dfs(int n, int floor) {
+		int left = 0, right = 0;
+		
+		if (tree[n][0] != -1) {
+			left = dfs(tree[n][0], floor + 1);
+		}
+		
+		if (tree[n][1] != -1) {
+			right = dfs(tree[n][1], floor + 1);
+		}
+		return left + right + 1;
 	}
 
 }
