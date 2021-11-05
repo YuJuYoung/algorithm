5dabe467068b9834b205275243c9cda9f188b22a
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2021-11-05 22:57:45 +0900

    1102번 풀이중

--- 백준 알고리즘/src/DP/Problem_1102.java
+++ 백준 알고리즘/src/DP/Problem_1102.java
@@ -3,12 +3,18 @@ package DP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Problem_1102 {
 	
-	private static int N, P;
+	private static int N, P, start;
 	private static int[][] costs;
+	private static char[] YN;
+	
+	private static int[][][] dp;
+	private static boolean[][] visited;
+	private static List<Integer> broke;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,6 +28,34 @@ public class Problem_1102 {
 			for (int j = 0; j < N; j++) {
 				costs[i][j] = Integer.parseInt(st.nextToken());
 			}
+		}
+		
+		YN = br.readLine().toCharArray();
+		P = Integer.parseInt(br.readLine());
+		start = 0;
+		
+		for (int i = 0; i < N; i++) {
+			if (YN[i] == 'N') {
+				broke.add(i);
+			} else {
+				start += 1 << i;
+			}
+		}
+		
+		int min = P - N - broke.size();
+		
+		if (min <= 0) {
+			System.out.println(0);
+		} else {
+			int len = broke.size();
+			
+			dp = new int[len][min + 1][2];
+			visited = new boolean[len][min + 1];
+			
+			for (int i = 0; i < len; i++) {
+				int index = broke.get(i);
+			}
+			System.out.println(visited[len - 1][min] ? dp[len - 1][min][1] : -1);
 		}
 	}
 
