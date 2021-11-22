adbce7bfef0042a0c18d0a1779011a7ff814ad49
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2021-11-22 21:28:13 +0900

    2342번 풀이중

--- 백준 알고리즘/src/DP/Problem_2342.java
+++ 백준 알고리즘/src/DP/Problem_2342.java
@@ -6,41 +6,74 @@ import java.io.InputStreamReader;
 
 public class Problem_2342 {
 	
-	private static int[] seq;
-	private static int seqLen, ans;
+	private static String seq;
+	private static int len;
+	private static int[][] pows;
+	private static int[][][] dp;
 
 	public static void main(String[] args) throws IOException {
-		setVar();
-		solve();
-		printAns();
+		setField();
+		System.out.println(solve());
 	}
 	
-	private static void setVar() throws IOException {
+	private static void setField() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String str = br.readLine();
-		seqLen = str.length() / 2;
-		
-		if (seqLen == 0) {
+		if ((seq = br.readLine()).length() == 1) {
 			return;
 		}
-		seq = new int[seqLen];
+		len = seq.length() / 2;
+		pows = new int[5][5];
+		dp = new int[len][5][5];
 		
-		for (int i = 0; i < seq.length; i++) {
-			seq[i] = str.charAt(i * 2) - 48;
+		for (int i = 1; i <= 4; i++) {
+			pows[0][i] = pows[i][0] = 2;
+			pows[i][i] = 1;
 		}
+		pows[1][2] = pows[2][1] = 3;
+		pows[2][3] = pows[3][2] = 3;
+		pows[3][4] = pows[4][3] = 3;
+		pows[1][4] = pows[4][1] = 3;
+		pows[1][3] = pows[3][1] = 4;
+		pows[2][4] = pows[4][2] = 4;
 	}
 	
-	private static void solve() {
-		if (seq == null) {
-			ans = 0;
-		} else {
-			int[][][] dp = new int[seqLen][5][5];
+	private static int solve() {
+		if (len == 0) {
+			return 0;
 		}
-	}
-	
-	private static void printAns() {
-		System.out.println(ans);
+		int last, cur;
+		
+		cur = seq.charAt(0) - 48;
+		dp[0][0][cur] = dp[0][cur][0] = 1;
+		
+		for (int i = 1; i < len; i++) {
+			last = seq.charAt((i - 1) * 2) - 48;
+			cur = seq.charAt(i * 2) - 48;
+			
+			
+			for (int j = 0; j < 5; j++) {
+				int min;
+				
+				if (dp[i - 1][last][j] != 0) {
+					min = Math.min(dp[i][cur][j], dp[i - 1][last][j] + pows[last][cur]);
+					dp[i][cur][j] = dp[i][j][cur] = min;
+					
+					min = Math.min(dp[i][last][cur], dp[i - 1][last][j] + pows[j][cur]);
+					dp[i][last][cur] = dp[i][cur][last] = min;
+				}
+			}
+		}
+		
+		int min = Integer.MAX_VALUE;
+		int end = seq.charAt((len - 1) * 2) - 48;
+		
+		for (int i = 0; i < 5; i++) {
+			if (min > dp[len - 1][end][i]) {
+				min = dp[len - 1][end][i];
+			}
+		}
+		return min;
 	}
 
 }
