25e6193c1da363c6483d212a56485634ef708f5c
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2021-11-26 21:01:05 +0900

    8872번 풀이중

--- 백준 알고리즘/src/DFS/Problem_8872.java
+++ 백준 알고리즘/src/DFS/Problem_8872.java
@@ -8,10 +8,16 @@ import java.util.StringTokenizer;
 public class Problem_8872 {
 	
 	private static int N, M, L;
+	
+	private static int[] group, groupMax;
 	private static Node[] graph;
 
 	public static void main(String[] args) throws IOException {
 		setVar();
+		System.out.println(solve());
+		for (int num : groupMax) {
+			System.out.println(num);
+		}
 	}
 	
 	private static void setVar() throws IOException {
@@ -21,9 +27,12 @@ public class Problem_8872 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		L = Integer.parseInt(st.nextToken());
+		
+		groupMax = new int[N - M + 1];
+		group = new int[N];
 		graph = new Node[N];
 		
-		while (M-- > 0) {
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			int A = Integer.parseInt(st.nextToken());
@@ -35,10 +44,40 @@ public class Problem_8872 {
 		}
 	}
 	
-	private static void solve() {
+	private static int solve() {
+		int groupNum = 1;
+		
 		for (int i = 0; i < N; i++) {
-			
+			if (group[i] == 0) {
+				group[i] = groupNum++;
+			}
+			groupMax[group[i]] = dfs(i, -1, group[i]);
 		}
+		
+		int ans = L * (N - M - 1);
+		
+		for (int i = 1; i <= N - M; i++) {
+			ans += groupMax[i];
+		}
+		return ans;
+	}
+	
+	private static int dfs(int n, int last, int groupNum) {
+		group[n] = groupNum;
+		
+		int max = 0;
+		
+		for (Node node = graph[n]; node != null; node = node.next) {
+			if (node.n == last) {
+				continue;
+			}
+			
+			if (node.cache == 0) {
+				node.cache = dfs(node.n, n, groupNum) + node.t;
+			}
+			max = Math.max(max, node.cache);
+		}
+		return max;
 	}
 	
 	private static class Node {
