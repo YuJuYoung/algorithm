80b99f7c89e52741efe2d7d4b14524bde1d679f7
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2022-02-02 20:49:20 +0900

    1236번 정답

---
+++ 백준 알고리즘/src/구현/Problem_1236.java
@@ -0,0 +1,62 @@
+package ±¸Çö;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Problem_1236 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		char[][] map = new char[N][M];
+		
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			
+			for (int j = 0; j < M; j++) {
+				map[i][j] = str.charAt(j);
+			}
+		}
+		
+		int a = 0, b = 0;
+		
+		for (int i = 0; i < N; i++) {
+			boolean blocked = false;
+			
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] == 'X') {
+					blocked = true;
+					break;
+				}
+			}
+			
+			if (!blocked) {
+				a++;
+			}
+		}
+		
+		for (int j = 0; j < M; j++) {
+			boolean blocked = false;
+			
+			for (int i = 0; i < N; i++) {
+				if (map[i][j] == 'X') {
+					blocked = true;
+					break;
+				}
+			}
+			
+			if (!blocked) {
+				b++;
+			}
+		}
+		System.out.println(Math.max(a, b));
+	}
+
+}
