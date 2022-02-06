9844e9bc7a91ef04cbf41df496e92cf945625600
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2022-02-06 20:44:01 +0900

    1300번 풀이중

---
+++ 백준 알고리즘/src/이분탐색/Problem_1300.java
@@ -0,0 +1,15 @@
+package ÀÌºÐÅ½»ö;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_1300 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+	}
+
+}
