576f746a05f986e0366e431b9b4e7b6b8222907a
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2022-02-06 20:44:10 +0900

    1300번 풀이중

--- 백준 알고리즘/src/이분탐색/Problem_1300.java
+++ 백준 알고리즘/src/이분탐색/Problem_1300.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Problem_1300 {
+	
+	private static int N, k;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		
+		N = Integer.parseInt(br.readLine());
+		k = Integer.parseInt(br.readLine());
 	}
 
 }
