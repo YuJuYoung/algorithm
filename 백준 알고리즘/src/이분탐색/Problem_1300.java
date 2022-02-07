7cd36f44face24d3d7de6d68900d18f1e0aeaac5
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2022-02-07 19:29:10 +0900

    1300번 정답

--- 백준 알고리즘/src/이분탐색/Problem_1300.java
+++ 백준 알고리즘/src/이분탐색/Problem_1300.java
@@ -6,14 +6,48 @@ import java.io.InputStreamReader;
 
 public class Problem_1300 {
 	
-	private static int N, k;
+	private static long N, k;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		N = Integer.parseInt(br.readLine());
-		k = Integer.parseInt(br.readLine());
+		N = Long.parseLong(br.readLine());
+		k = Long.parseLong(br.readLine());
 		
+		System.out.println(binarysearch(1, N * N, k));
+	}
+	
+	private static long binarysearch(long s, long e, long k) {
+		while (s < e) {
+			long m = (s + e) / 2;
+			
+			long[] count = getCount(m);
+			long tot = count[0];
+			long same = count[1];
+			
+			if (tot < k) {
+				s = m + 1;
+			} else {
+				if (tot - same < k) {
+					return m;
+				}
+				e = m;
+			}
+		}
+		return s;
+	}
+	
+	private static long[] getCount(long m) {
+		long[] count = new long[2];
+		
+		for (int i = 1; i <= N && i <= m; i++) {
+			count[0] += Math.min(N, m / i);
+			
+			if (m % i == 0 && m / i <= N) {
+				count[1]++;
+			}
+		}
+		return count;
 	}
 
 }
