7c1cf5e89c060bea7075cef612ac605821f07834
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2022-02-11 22:48:01 +0900

    16287번 풀이중

--- 백준 알고리즘/src/DP/Problem_16287.java
+++ 백준 알고리즘/src/DP/Problem_16287.java
@@ -9,7 +9,6 @@ import java.util.StringTokenizer;
 public class Problem_16287 {
 	
 	private static int w, n;
-	
 	private static int[] A;
 	private static boolean[][] dp;
 
@@ -21,7 +20,7 @@ public class Problem_16287 {
 		n = Integer.parseInt(st.nextToken());
 		
 		A = new int[n];
-		dp = new boolean[w + 1][4];
+		dp = new boolean[w + 1][5];
 		
 		st = new StringTokenizer(br.readLine());
 		
@@ -30,33 +29,33 @@ public class Problem_16287 {
 		}
 		Arrays.sort(A);
 		
-		System.out.println(dfs(-1, 0, 0) ? "YES" : "NO");
-	}
-	
-	private static boolean dfs(int last, int sum, int cnt) {
-		dp[sum][cnt] = true;
+		dp[0][0] = true;
 		
-		for (int i = last + 1; i <= n - 4 + cnt; i++) {
-			int nSum = sum + A[i];
+		for (int i = 0; i < n; i++) {
+			int weight = A[i];
 			
-			if (nSum > w) {
-				break;
-			}
-			
-			if (cnt == 3) {
-				if (nSum == w) {
-					return true;
-				}
-				return false;
-			} else {
-				if (!dp[nSum][cnt + 1]) {
-					if (dfs(i, nSum, cnt + 1)) {
-						return true;
+			for (int j = Math.min(w, getMax(i) + weight); j - weight >= 0; j--) {
+				for (int k = 0; k <= 3; k++) {
+					if (dp[j - weight][k]) {
+						dp[j][k + 1] = true;
 					}
 				}
 			}
 		}
-		return false;
+		System.out.println(dp[w][4] ? "YES" : "NO");
+	}
+	
+	private static int getMax(int i) {
+		if (i == 0) {
+			return 0;
+		}
+		if (i == 1) {
+			return A[0];
+		}
+		if (i == 2) {
+			return A[0] + A[1];
+		}
+		return A[i - 3] + A[i - 2] + A[i - 1];
 	}
 
 }
