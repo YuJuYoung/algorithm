8fca599ad6530fd6a2b412da595295c26af9c75c
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2022-02-14 19:13:01 +0900

    1072번 정답

---
+++ 백준 알고리즘/src/이분탐색/Problem_1072.java
@@ -0,0 +1,42 @@
+package ÀÌºÐÅ½»ö;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem_1072 {
+	
+	private static long X, Y, Z;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		X = Integer.parseInt(st.nextToken());
+		Y = Integer.parseInt(st.nextToken());
+		Z = getZ(0);
+		
+		System.out.println(binarysearch());
+	}
+	
+	private static long binarysearch() {
+		long s = 1, e = X;
+		
+		while (s < e) {
+			long m = (s + e) / 2;
+			
+			if (getZ(m) > Z) {
+				e = m;
+			} else {
+				s = m + 1;
+			}
+		}
+		return getZ(s) == Z ? -1 : s;
+	}
+	
+	private static long getZ(long n) {
+		return (Y + n) * 100 / (X + n);
+	}
+
+}
