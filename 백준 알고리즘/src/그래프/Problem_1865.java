ab146586feccd186b7e0870a24ea3e16028b02a8
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2022-02-19 23:15:08 +0900

    1865번 정답

--- 백준 알고리즘/src/그래프/Problem_1865.java
+++ 백준 알고리즘/src/그래프/Problem_1865.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
 public class Problem_1865 {
 	
 	private static int N, M, W;
-	private static int[][] road, hole;
+	private static Node[] graph;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,71 +23,74 @@ public class Problem_1865 {
 			M = Integer.parseInt(st.nextToken());
 			W = Integer.parseInt(st.nextToken());
 			
-			road = new int[M * 2 + 1][3];
-			hole = new int[W + 1][3];
+			graph = new Node[N + 1];
 			
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
 				
-				for (int j = 0; j < 3; j++) {
-					road[i * 2][j] = Integer.parseInt(st.nextToken());
-				}
-				road[i * 2 + 1][0] = road[i * 2][1];
-				road[i * 2 + 1][1] = road[i * 2][0];
-				road[i * 2 + 1][2] = road[i * 2][2];
+				int s = Integer.parseInt(st.nextToken());
+				int e = Integer.parseInt(st.nextToken());
+				int t = Integer.parseInt(st.nextToken());
+				
+				graph[s] = new Node(e, t, graph[s]);
+				graph[e] = new Node(s, t, graph[e]);
 			}
 			
 			for (int i = 0; i < W; i++) {
 				st = new StringTokenizer(br.readLine());
 				
-				for (int j = 0; j < 2; j++) {
-					hole[i][j] = Integer.parseInt(st.nextToken());
-				}
-				hole[i][2] = -Integer.parseInt(st.nextToken());
+				int s = Integer.parseInt(st.nextToken());
+				int e = Integer.parseInt(st.nextToken());
+				int t = Integer.parseInt(st.nextToken());
+				
+				graph[s] = new Node(e, -t, graph[s]);
 			}
-			bw.write(getAnswer());
+			
+			bw.write(solve());
 			bw.newLine();
 		}
 		bw.close();
 	}
 	
-	private static String getAnswer() {
-		for (int t = 1; t <= N; t++) {
-			int[] dist = new int[N + 1];
+	private static String solve() {
+		int[] time = new int[N + 1];
+		boolean updated = false;
+		
+		for (int i = 0; i < N; i++) {
+			int[] temp = new int[N + 1];
+			updated = false;
 			
-			for (int i = 1; i <= N; i++) {
-				if (i != t) {
-					dist[i] = Integer.MAX_VALUE;
+			for (int j = 1; j <= N; j++) {
+				temp[j] = time[j];
+			}
+			
+			for (int n = 1; n <= N; n++) {
+				for (Node next = graph[n]; next != null; next = next.next) {
+					int nt = temp[n] + next.t;
+					
+					if (temp[next.n] > nt) {
+						temp[next.n] = nt;
+						updated = true;
+					}
 				}
 			}
 			
-			for (int i = 0; i < N; i++) {
-				int[] temp = new int[N + 1];
-				
-				for (int j = 1; j <= N; j++) {
-					temp[j] = dist[j];
-				}
-				setTemp(dist, temp, road);
-				setTemp(dist, temp, hole);
-				
-				if (temp[t] < 0) {
-					return "YES";
-				}
-				dist = temp;
+			if (!updated) {
+				break;
 			}
+			time = temp;
 		}
-		return "NO";
+		return updated ? "YES" : "NO";
 	}
 	
-	private static void setTemp(int[] dist, int[] temp, int[][] arr) {
-		for (int[] info : arr) {
-			int s = info[0];
-			int e = info[1];
-			int t = info[2];
-			
-			if (dist[s] != Integer.MAX_VALUE) {
-				temp[e] = Math.min(temp[e], dist[s] + t);
-			}
+	private static class Node {
+		int n, t;
+		Node next;
+		
+		public Node(int n, int t, Node next) {
+			this.n = n;
+			this.t = t;
+			this.next = next;
 		}
 	}
 
