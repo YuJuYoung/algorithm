583cc4f7527f50f35bce686bb19e54715504aa58
wnqhd <wnqhd@DESKTOP-O3F4QPT>
2022-03-05 15:38:41 +0900

    9376번 정답

--- 백준 알고리즘/src/BFS/Problem_9376.java
+++ 백준 알고리즘/src/BFS/Problem_9376.java
@@ -5,17 +5,15 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Problem_9376 {
 	
 	private static int h, w;
 	private static char[][] map;
-	private static int[][][] cache;
-	
-	private static int[] dx = { 0, -1, 0, 1 };
-	private static int[] dy = { 1, 0, -1, 0 };
+	private static int[][][] counts;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,91 +22,78 @@ public class Problem_9376 {
 		for (int t = Integer.parseInt(br.readLine()); t > 0; t--) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			h = Integer.parseInt(st.nextToken()) + 2;
-			w = Integer.parseInt(st.nextToken()) + 2;
-			map = new char[h][w];
-			cache = new int[h][w][3];
+			h = Integer.parseInt(st.nextToken());
+			w = Integer.parseInt(st.nextToken());
+			map = new char[h + 2][w + 2];
+			counts = new int[h + 2][w + 2][3];
 			
-			Point[] start = new Point[3];
-			int idx = 1;
-			for (int i = 1; i < h - 1; i++) {
+			Deque<int[]> dq = new LinkedList<>();
+			int idx = 0;
+			
+			for (int i = 1; i <= h; i++) {
 				String str = br.readLine();
 				
-				for (int j = 1; j < w - 1; j++) {
+				for (int j = 1; j <= w; j++) {
 					map[i][j] = str.charAt(j - 1);
 					
 					if (map[i][j] == '$') {
-						start[idx++] = new Point(j, i, 0);
+						dq.add(new int[] { i, j, 1, idx });
+						counts[i][j][idx++] = 1;
 					}
 				}
 			}
-			start[0] = new Point(0, 0, 0);
+			dq.add(new int[] { 0, 0, 1, idx });
+			counts[0][0][idx] = 1;
 			
-			for (int i = 0; i < 3; i++) {
-				bfs(start[i], i);
-			}
-			
-			int ans = Integer.MAX_VALUE;
-			for (int i = 0; i < h; i++) {
-				for (int j = 0; j < w; j++) {
-					if (map[i][j] == '*') {
-						continue;
-					}
-					int sum = cache[i][j][0] + cache[i][j][1] + cache[i][j][2];
-					
-					if (map[i][j] == '#') {
-						sum -= 2;
-					}
-					ans = Math.min(ans, sum);
-				}
-			}
-			bw.write(ans + "\n");
+			bw.write(solve(dq) + "\n");
 		}
 		bw.close();
 	}
 	
-	private static void bfs(Point start, int idx) {
-		boolean[][] visited = new boolean[h][w];
+	private static int solve(Deque<int[]> dq) {
+		int min = Integer.MAX_VALUE;
 		
-		ArrayDeque<Point> q = new ArrayDeque<>();
-		q.add(start);
-		visited[start.y][start.x] = true;
+		int[] dx = { 0, 1, -1, 0 };
+		int[] dy = { 1, 0, 0, -1 };
 		
-		while (!q.isEmpty()) {
-			Point p = q.pollFirst();
-			int x = p.x;
-			int y = p.y;
-			int c = p.c;
-			cache[y][x][idx] = c;
+		while (!dq.isEmpty()) {
+			int[] node = dq.pollFirst();
+			
+			int x = node[1];
+			int y = node[0];
+			int count = node[2];
+			int idx = node[3];
+			
+			if (counts[y][x][0] != 0 && counts[y][x][1] != 0 && counts[y][x][2] != 0) {
+				int sum = counts[y][x][0] + counts[y][x][1] + counts[y][x][2] - 3;
+				
+				if (map[y][x] == '#') {
+					min = Math.min(min, sum - 2);
+				} else {
+					min = Math.min(min, sum);
+				}
+			}
 			
 			for (int i = 0; i < 4; i++) {
 				int nx = x + dx[i];
 				int ny = y + dy[i];
 				
-				if (isInside(nx, ny) && map[ny][nx] != '*' && !visited[ny][nx]) {
+				if (isInside(nx, ny) && map[ny][nx] != '*' && counts[ny][nx][idx] == 0) {
 					if (map[ny][nx] == '#') {
-						q.addLast(new Point(nx, ny, c + 1));
+						dq.addLast(new int[] { ny, nx, count + 1, idx });
+						counts[ny][nx][idx] = count + 1;
 					} else {
-						q.addFirst(new Point(nx, ny, c));
+						dq.addFirst(new int[] { ny, nx, count, idx });
+						counts[ny][nx][idx] = count;
 					}
-					visited[ny][nx] = true;
 				}
 			}
 		}
+		return min;
 	}
 	
 	private static boolean isInside(int x, int y) {
-		return x >= 0 && x < w && y >= 0 && y < h;
-	}
-	
-	private static class Point {
-		int x, y, c;
-		
-		public Point(int x, int y, int c) {
-			this.x = x;
-			this.y = y;
-			this.c = c;
-		}
+		return !(x < 0 || x > w + 1 || y < 0 || y > h + 1);
 	}
 
 }
